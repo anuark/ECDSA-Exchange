e3974279bf6c6826cb8ae3af675bdee7b15e5ad2
Anuar <anuar.kilgore@buyersedgeplatform.com>
2021-10-10 20:47:22 -0600

    public key wallets and sign/verify

--- client/index.js
+++ client/index.js
@@ -1,4 +1,7 @@
 import "./index.scss";
+import { ec as EC } from 'elliptic';
+const ec = new EC('secp256k1');
+import SHA256 from 'crypto-js/sha256';
 
 const server = "http://localhost:3042";
 
@@ -19,9 +22,13 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privateKey = document.getElementById("privateKey").value;
+  const key = ec.keyFromPrivate(privateKey);
+  const msgHash = SHA256(JSON.stringify({ sender, amount, recipient }));
+  const signature = key.sign(msgHash.toString());
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, signature, hash: msgHash.toString()
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
--- server/index.js
+++ server/index.js
@@ -3,15 +3,28 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const key = ec.genKeyPair();
+const SHA256 = require('crypto-js/sha256');
+
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+const defaultBalances = [100, 50, 75];
+const balances = {};
+for (let i=0; i<3; i++) {
+    const key = ec.genKeyPair();
+    const publicKey = key.getPublic().encode('hex');
+    const privateKey = key.getPrivate().toBuffer().toString('hex')
+
+    balances[publicKey] = defaultBalances[i];
+    console.log(`Account ${i} balance: ${defaultBalances[i]}`);
+    console.log(`Public Key: ${publicKey}`);
+    console.log(`Private Key: ${privateKey}`);
+    console.log("");
 }
 
 app.get('/balance/:address', (req, res) => {
@@ -21,12 +34,21 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
+  const {sender, recipient, amount, signature, hash} = req.body;
+  
+  const publicKey = ec.keyFromPublic(sender, 'hex');
+
+  if (!publicKey.verify(hash, signature)) {
+      return res.status(400).send({ message: 'invalid signature' });
+  }
+
   balances[sender] -= amount;
   balances[recipient] = (balances[recipient] || 0) + +amount;
+
   res.send({ balance: balances[sender] });
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
+
